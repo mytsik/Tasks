b9ab20df885ac96893008431e8ca06e8750e5465
mytsik <mycikyanina@gmail.com>
2024-09-08 22:16:09 +0300

    Домашнее задание

---
+++ Homework1/HW1.c
@@ -0,0 +1,25 @@
+
+
+
+#include <locale.h>
+void main()
+
+{
+    puts("    |\\");
+	puts("    | \\");
+	puts("    |  \\");
+	puts("    |  /");
+	puts("    | /");
+	puts("    |/");
+	puts("____|_____");
+	puts("\\________/");
+
+
+	puts("    _");
+	puts("   / \\");
+	puts("  /   \\");
+	puts(" /-----\\");
+	puts("/|  _  |\\");
+	puts(" | |_| |");
+	puts(" |_____|");	
+}
